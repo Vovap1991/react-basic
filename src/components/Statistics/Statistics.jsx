a95ca0c76545fab7e83e77dbe4450b1459e9b287
YuraIvashkiv <yuraivashkiv1995@gmail.com>
2023-08-13 16:47:14 +0300

    task-2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,11 +3,22 @@ import { StatisticsList, StatisticTitle } from './Statistics.styled';
 import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
+const icons = [
+  <FaRegThumbsUp/>,
+  <MdPeople/>,
+  <MdOutlineProductionQuantityLimits/>,
+  <GiTreeDoor/>,
+];
 
-export const Statistics = () => {
+export const Statistics = ({ title, stats }) => {
   return (
     <>
-      <div>StatisticsList</div>
+      { title  && < StatisticTitle > { title }</StatisticTitle >}
+      <StatisticsList>
+        {stats.map((stat, index) => (
+          <StatisticItem icon={icons[index]} statItem={stat} key={stat.id} />
+        ))}
+      </StatisticsList>
     </>
   );
 };
