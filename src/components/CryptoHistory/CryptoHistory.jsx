f3c243f94405c24c975c7f69f341b0655f2c54b5
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-08-28 19:24:39 +0300

    task 4 is done

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,28 @@
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
+import { format } from 'date-fns';
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+export const CryptoHistory = ({ items }) => {
+  return (
+    <BaseTable>
+      <THead>
+        <tr>
+          <Th>№</Th>
+          <Th>PRICE</Th>
+          <Th>AMOUNT</Th>
+          <Th>DATE</Th>
+        </tr>
+      </THead>
+
+      <tbody>
+        {items.map(({ id, price, amount, date }, index) => (
+          <Tr key={id}>
+            <Td>{index + 1}</Td>
+            <Td>{price}</Td>
+            <Td>{amount}</Td>
+            <Td>{format(new Date(`${date}`), 'MM/dd/yyyy, h:m a')}</Td>
+          </Tr>
+        ))}
+      </tbody>
+    </BaseTable>
+  );
 };
